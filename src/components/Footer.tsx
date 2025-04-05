b2114cadf1cd9c2104efc7d71e50b36a291c36ba
vLimovs <limonytchannel@gmail.com>
2025-04-05 23:40:10 +0500

    Added EmailJS lib

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,45 @@
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
+import toastr from 'toastr';
+
 const Footer = () => {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": true,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": undefined,
+        "showDuration": 300,
+        "hideDuration": 1000,
+        "timeOut": 3000,
+        "extendedTimeOut": 1000,
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+    const form = useRef<HTMLFormElement>(null);
+    const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (!form.current) return;
+
+        emailjs
+            .sendForm('service_ov51aww', 'template_qhif8qh', form.current, {
+                publicKey: '5U5PEflivOqyvVx2d',
+            })
+            .then(
+                () => {
+                    toastr["success"](" ", "The message has sent successfully !");
+                },
+                () => {
+                    toastr["error"](" ", "Something went wrong :с");
+                },
+            );
+    };
+
     return (
         <footer className="footer">
             <div className="container">
@@ -7,18 +48,18 @@ const Footer = () => {
                     <p>Frontend Developer with 1 year of hands-on experience in building responsive, dynamic, and user-friendly web applications. Skilled in crafting modern interfaces using HTML, CSS, SCSS, and JavaScript, with expertise in React.js, Next.js, and TypeScript to develop scalable and high-performance solutions.</p>
                     <a href="mailto:vlimovs@icloud.com"><img src="mail.svg" alt="" />vlimovs@icloud.com</a>
                 </div>
-                <form id="form" className="footer__form">
+                <form id="form" ref={form} onSubmit={sendEmail} className="footer__form">
                     <label>Your name</label>
-                    <input type="text" placeholder="Your name" required />
+                    <input type="text" name="user_name" placeholder="Your name" required />
                     <label>Your email</label>
-                    <input type="email" placeholder="Your email" required/>
+                    <input type="email" name="user_email" placeholder="Your email" required />
                     <label>Type a message</label>
-                    <textarea style={{resize: 'none'}} placeholder="Text..." required/>
-                    <button type="submit">Submit</button>
+                    <textarea name="message" style={{ resize: 'none' }} placeholder="Text..." required />
+                    <button type="submit" value="Send">Submit</button>
                 </form>
             </div>
         </footer>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
